bbb0d8b621c664a07d4b54516de6a49d91038386
NateSrkn <natesrkn@gmail.com>
2020-04-14 00:10:43 -0400

    cors added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 
 const port = 3000 || process.env.PORT
@@ -14,11 +15,20 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopo
 const usersRoute = require('./routes/user.js')
 const authRoute = require('./routes/auth.js')
 const postsRoute = require('./routes/posts')
-
+app.use(cors({
+  credentials: true,
+  origin: true
+}))
+app.options('*', cors())
 app.use(express.json())
 app.use('/api/users', usersRoute)
 app.use('/api/auth', authRoute)
 app.use('/api/posts', postsRoute)
+app.use('/', (req, res) => {
+  res.json({
+    message: 'Hello!'
+  })
+})
 
 app.listen(port, () => {
   console.log(`Now running on ${port}`)
